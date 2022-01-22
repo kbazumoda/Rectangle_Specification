e19fa341bd933b520dd23d4bfd3af07ed41c4d9c
medof <medof@DESKTOP-ID9370A>
2022-01-22 11:11:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rectangle_Specification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
